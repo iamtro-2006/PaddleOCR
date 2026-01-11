ecf076c0d8bf33560f0955b33bbeb39768c470f9
iamtro <nguyentan121117@gmail.com>
2026-01-12 01:02:47 +0700

    paddleocr-iamtro

--- main.py
+++ main.py
@@ -1,42 +1,17 @@
 import os
-import cv2
 import time
 import json
 import numpy as np
 from pathlib import Path
 from tqdm import tqdm
-from torch.utils.data import Dataset, DataLoader 
-from paddleocr import PaddleOCR 
-
-class ImageDataset(Dataset):
-    def __init__(self, image_dir):
-        self.image_paths = sorted(list(Path(image_dir).glob("*.jpg")))
-    def __len__(self):
-        return len(self.image_paths)
-    def __getitem__(self, idx):
-        path = self.image_paths[idx]
-        img = cv2.imread(str(path))
-        if img is None:
-            return None
-        return str(path), img
-
-def collate_fn(batch):
-    return [item for item in batch if item is not None]
-
-def build_loader(image_dir, batch_size, num_workers, shuffle=False):
-    inference_dataset = ImageDataset(image_dir)
-    inference_loader = DataLoader(inference_dataset, 
-                                  batch_size=batch_size, 
-                                  num_workers=num_workers,
-                                  collate_fn=collate_fn,
-                                  shuffle=shuffle)
-    return inference_loader, len(inference_dataset) 
+from paddleocr import PaddleOCR
+from my_dataset import ImageDataset
+from config import CFG
+from modules import build_loader, collate_fn
+ 
 
 def main():
-    folder = Path(r"E:\VBS-DATA\keyframes\V3C")
-    batch_size = 16 
-    num_workers = 0 
-
+    folder = Path(CFG.folder_path_in)
 
     model = PaddleOCR(ocr_version="PP-OCRv5",
                       text_detection_model_name="PP-OCRv5_mobile_det",
@@ -51,40 +26,46 @@ def main():
         if not image_dir.is_dir():
             continue
         
+        image_dir_name = image_dir.name
+        
+        if int(image_dir_name) < CFG.start or int(image_dir_name) > CFG.end :
+            continue
+        
         dataloader, total_images = build_loader(image_dir=image_dir,
-                                               batch_size=batch_size,
-                                               num_workers=num_workers)
+                                                batch_size=CFG.batch_size,
+                                                num_workers=CFG.num_workers)
         if total_images == 0:
             continue
 
         results_to_save = []
         start = time.time()
         
-        for batch in tqdm(dataloader, desc=f"OCR {image_dir.name}", leave=False):
+        for batch in tqdm(dataloader, desc=f"OCR {image_dir_name}", leave=False):
             if not batch: 
                 continue
 
             paths, imgs = zip(*batch)
             results = model.predict(list(imgs))
-             
+            
             if results is None: 
                 continue
-
+            
             for path, res in zip(paths, results):
                 filename = Path(path).stem
                 texts = []
                 if res:
                     texts = res["rec_texts"]
                 results_to_save.append([filename, texts])
-                res.save_to_img(f"/root/res/visuals/{filename}.jpg")
+                if CFG.visualized:
+                    res.save_to_img(f"{CFG.folder_path_out}/visuals/{image_dir_name}/{filename}.jpg")
 
-        output_file = f"/root/res/V3C/{image_dir.name}.json"
+        output_file = f"{CFG.folder_path_out}/{image_dir_name}.json"
         os.makedirs(os.path.dirname(output_file), exist_ok=True)
         with open(output_file, "w", encoding="utf-8") as f:
             json.dump(results_to_save, f, ensure_ascii=False, indent=2)
 
         elapsed = round(time.time() - start, 2)
-        print(f"\nFinished {image_dir.name}: {elapsed}s for {total_images} images.")
+        print(f"\nFinished {image_dir_name}: {elapsed}s for {total_images} images.")
 
 if __name__ == "__main__":
     main()
